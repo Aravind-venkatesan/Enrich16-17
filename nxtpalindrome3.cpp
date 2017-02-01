d93ca0eb8f3aa9df3d5cdf7410a13b88002e19a4
Aravind-venkatesan <aravind1303.av@gmail.com>
2017-02-01 20:46:56 +0530

    Create nxtpalindrome3.cpp

---
+++ nxtpalindrome3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a[15],num,size=0,lim,i,j;
+	cout<<"enter the number";
+	cin>>num;
+	num=num+1;
+	while(num>0)
+	{
+		a[size]=num%10;
+		num=num/10;
+		size++;
+	}
+	lim=size/2;
+	for(i=0;i<=lim;i++)
+	{
+		if(a[i]>a[size-i-1])
+		{
+			a[i+1]=a[i+1]+1;
+			j=i+1;
+			while(a[j]==10)
+			{
+				a[j]=0;
+				a[j+1]=a[j+1]+1;
+				j++;
+			}
+			a[i]=a[size-i-1];
+		}
+		else
+		{
+			a[i]=a[size-i-1];
+		} 
+	}
+	cout<<"\n";
+	for(j=0;j<size;j++)
+	{
+		cout<<a[j];
+	}
+	return 0;
+}
